c0c2d321348f7df79771acd94a2c2a40e049a7b4
Maddie <egonbold@gmx.de>
2025-04-09 19:08:15 +0200

    Nix generation: 12 current  2025-04-09 19:08:05  24.11.20250405.7819a0d     6.6.85                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -138,6 +138,7 @@
     xclicker
     rustup
     tealdeer
+    rustlings
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
