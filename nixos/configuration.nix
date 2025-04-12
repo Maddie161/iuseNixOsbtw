f253c5fc985ddedab363756381a41f1629e6b68b
Maddie <egonbold@gmx.de>
2025-04-12 13:03:10 +0200

    Nix generation: 14 current  2025-04-12 13:02:58  24.11.20250405.7819a0d     6.6.85                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -140,6 +140,7 @@
     tealdeer
     rustlings
     transmission_4-qt
+    bottles
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
