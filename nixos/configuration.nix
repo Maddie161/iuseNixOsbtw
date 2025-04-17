9d404ac1304ccc62bb2343ded423319f230ba7b8
Maddie <egonbold@gmx.de>
2025-04-17 15:50:34 +0200

    Nix generation: 16 current  2025-04-17 15:50:19  24.11.20250405.7819a0d     6.6.85                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -142,6 +142,7 @@
     transmission_4-qt
     bottles
     gamescope
+    prismlauncher
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
