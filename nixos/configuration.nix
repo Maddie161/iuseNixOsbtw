a483b95eda128d725c5366bbc4642a5f8a665c82
Maddie <egonbold@gmx.de>
2025-04-20 21:12:15 +0200

    Nix generation: 17 current  2025-04-20 21:12:02  24.11.20250405.7819a0d     6.6.85                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -143,6 +143,7 @@
     bottles
     gamescope
     prismlauncher
+    zig
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
