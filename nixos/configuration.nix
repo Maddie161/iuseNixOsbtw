18867800c0d3abd83ceb64688971376fbf93d2c7
Maddie <egonbold@gmx.de>
2025-04-22 20:44:06 +0200

    Nix generation: 21 current  2025-04-22 20:43:51  24.11.20250405.7819a0d     6.6.85                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -146,7 +146,6 @@
     zig
     gcc9
     clang
-    logitech-udev-rules
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
