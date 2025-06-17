dfe47e33ea8a1ab518649b7c920cc3fa12f67462
Maddie <egonbold@gmx.de>
2025-06-17 16:07:27 +0200

    Nix generation: 35 current  2025-06-17 16:06:33  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -156,10 +156,6 @@ environment.systemPackages = with pkgs; [
     gcc9
     clang
     steam-run
-    fuse
-    zenity
-    xorg.xhost
-    mesa-utils
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
