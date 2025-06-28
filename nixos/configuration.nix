b9887c741c38caca70465cadbad72c06f9458945
Maddie <egonbold@gmx.de>
2025-06-28 21:32:39 +0200

    Nix generation: 36 current  2025-06-17 17:56:59  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,6 +10,7 @@
     programs.nix-ld.libraries = with pkgs; [
       # Add any missing dynamic libraries for unpackaged programs
       # here, NOT in environment.systemPackages
+      sdl3
     ];
   hardware.bluetooth.enable = true;
   hardware.graphics = {
