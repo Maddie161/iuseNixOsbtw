12b7a84c1e0776e81e77ed580fe01f29d3068f1d
Maddie <egonbold@gmx.de>
2025-07-13 00:45:27 +0200

    Nix generation: 39 current  2025-07-13 00:07:04  25.05.20250515.e06158e     6.12.28                          *

---
+++ home-manager/hyprland.nix
@@ -0,0 +1,106 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  programs.kitty.enable = true;
+  # Variables for Hostnames
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      "$term" = ''
+        kitty bash -e "fastfetch"
+      '';
+      "$runner" = "wofi --show drun";
+      "$screenshotter" = "hyprshot --mode region --clipboard-only";
+
+      bind =
+        [
+          #focus moving
+          "$mod, H, movefocus, l"
+
+          "$mod, L, movefocus, r"
+
+          "$mod, K, movefocus, u"
+
+          "$mod, J, movefocus, d"
+
+          #window moving
+          "$mod SHIFT, H, movewindow, l"
+
+          "$mod SHIFT, L, movewindow, r"
+
+          "$mod SHIFT, K, movewindow, u"
+
+          "$mod SHIFT, J, movewindow, d"
+
+          "$mod CONTROL_L, L, resizeactive, -40 0"
+
+          "$mod CONTROL_L, H, resizeactive, 40 0"
+
+          "$mod CONTROL_L, K, resizeactive, 0 40"
+
+          "$mod CONTROL_L, J, resizeactive, 0 -40"
+
+          "$mod, T, exec, $term"
+
+          "$mod, F, togglefloating"
+
+          "$mod SHIFT, F, fullscreen"
+
+          "$mod, Y, exec, kitty --class btop --hold -e btop"
+
+          "$mod, B, exec, firefox"
+
+          "$mod, D, exec, $runner"
+
+          "$mod SHIFT, S, exec, $screenshotter"
+
+          "$mod, Q, killactive,"
+        ]
+        ++ (
+          # workspaces
+          # binds $mod + [shift +] {1..9} to [move app to] workspace {1..9}
+          # also binds $mod {1..9} to move user to workspace {1..9}
+          builtins.concatLists (builtins.genList (
+              i: let
+                ws = i + 1;
+              in [
+                "$mod, code:1${toString i}, workspace, ${toString ws}"
+                "$mod SHIFT, code:1${toString i}, movetoworkspacesilent, ${toString ws}"
+              ]
+            )
+            9)
+        );
+      input = {
+        kb_layout = [
+          "de"
+        ];
+      };
+
+      # mouse bindings
+      bindm = [
+        # move window
+        "$mod, mouse:272, movewindow"
+        # resize
+        "$mod, mouse:273, resizewindow"
+        # alt left click to also resize
+        "$mod ALT, mouse:272, resizewindow"
+      ];
+      exec-once = [
+
+      ];
+      decoration = {
+        inactive_opacity = 0.9;
+      };
+      general = {
+        layout = "dwindle";
+        "col.active_border" = "rgb(111,48,158)";
+        border_size = 2;
+      };
+    };
+  };
+}
