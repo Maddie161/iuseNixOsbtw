587cf9ae21682a108c3226cfdba11a20b96cbb12
Maddie <egonbold@gmx.de>
2025-07-13 14:50:23 +0200

    Nix generation: 42 current  2025-07-13 14:50:12  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -168,6 +168,7 @@ environment.systemPackages = with pkgs; [
     kitty
     home-manager
     swaynotificationcenter
+    wlogout
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
