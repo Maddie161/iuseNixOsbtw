7cbc880091c6028dc72332202cbf3e85af640c16
Maddie <egonbold@gmx.de>
2025-07-13 18:20:48 +0200

    Nix generation: 45 current  2025-07-13 15:20:06  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -104,13 +104,13 @@
   security.rtkit.enable = true;
   services.pulseaudio.enable = true;
   services.pipewire = {
-    enable = true;
+    enable = false;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     services.pulseaudio.support32Bit = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
