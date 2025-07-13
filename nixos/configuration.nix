cc1a69243812edfde77faf058a40e7dd935acf24
Maddie <egonbold@gmx.de>
2025-07-13 14:41:07 +0200

    Nix generation: 40 current  2025-07-13 00:48:08  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -167,6 +167,7 @@ environment.systemPackages = with pkgs; [
     steam-run
     kitty
     home-manager
+    swaync
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
