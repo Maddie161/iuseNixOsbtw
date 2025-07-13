6ea3be8832d2704927c57cd1145ed42c43c1ac7d
Maddie <egonbold@gmx.de>
2025-07-13 19:17:44 +0200

    Nix generation: 50 current  2025-07-13 19:17:12  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -174,7 +174,6 @@ environment.systemPackages = with pkgs; [
     bluez-tools
     waybar
     pavucontrol
-    xdg-desktop-portal-hyprland
     polkit_gnome
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
