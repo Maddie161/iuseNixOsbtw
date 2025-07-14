8c58828eb7119f8f0bdc916a2d558d3386fdbc7c
Maddie <egonbold@gmx.de>
2025-07-14 17:04:30 +0200

    Nix generation: 50 current  2025-07-13 19:17:12  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -131,9 +131,10 @@
   services.dbus.enable = true;
   xdg.portal = {
     enable = true;
+    wlr.enable = false;
     extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
       xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
     ];
   };
 
