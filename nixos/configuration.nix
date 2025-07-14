9bbad49f5a610432e52616f30bd912e564c34671
Maddie <egonbold@gmx.de>
2025-07-14 15:37:07 +0200

    Nix generation: 50 current  2025-07-13 19:17:12  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -101,13 +101,14 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = true;
-  security.rtkit.enable = true;
-  services.pulseaudio.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
   services.pipewire = {
-    enable = false;
-    # If you want to use JACK applications, uncomment this
+    enable = true;
+    audio.enable = true;
+    alsa.enable = true;
     jack.enable = true;
+  };
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
