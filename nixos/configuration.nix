257f16d3394620eee54c6bc25dda9f7972b7477e
Maddie <egonbold@gmx.de>
2025-07-15 17:56:30 +0200

    Nix generation: 50 current  2025-07-13 19:17:12  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -190,6 +190,7 @@ environment.systemPackages = with pkgs; [
     xdg-desktop-portal-hyprland
     gdk-pixbuf
     hyprpaper
+    wofi
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
