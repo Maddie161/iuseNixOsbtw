60ed495157bcd7453ddd02a32b68da7bc632a1d3
Maddie <egonbold@gmx.de>
2025-07-15 19:07:40 +0200

    Nix generation: 50 current  2025-07-13 19:17:12  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -109,11 +109,9 @@
     alsa.enable = true;
     jack.enable = true;
   };
-
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
-  }
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -121,7 +119,6 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.maddie = {
     isNormalUser = true;
-    description = "Maddie";
     extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [
       kdePackages.kate
