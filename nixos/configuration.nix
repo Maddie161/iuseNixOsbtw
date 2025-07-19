c2a080092f060f651b011541f84802b787e28917
Maddie <egonbold@gmx.de>
2025-07-20 00:08:10 +0200

    Nix generation: 55 current  2025-07-20 00:07:40  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -187,6 +187,8 @@
     wofi
     hyprsunset
     hyprshot
+    fastfetch
+    hyfetch
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
