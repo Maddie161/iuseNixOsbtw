b52f515d249eef0142895f6aff4ae1e085f9c689
Maddie <egonbold@gmx.de>
2025-07-20 15:22:08 +0200

    Nix generation: 56 current  2025-07-20 15:06:18  25.05.20250515.e06158e     6.12.28                          *

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -21,4 +21,21 @@
       pull.rebase = "true";
     };
   };
+  programs.kitty = lib.mkForce {
+    enable = true;
+    settings = {
+      cursor_trail = 0;
+      cursor_shape = beam;
+      confirm_os_window_close = 0;
+      dynamic_background_opacity = true;
+      enable_audio_bell = false;
+      mouse_hide_wait = "-1.0";
+      window_padding_width = 10;
+      background_opacity = "0.5";
+      background_blur = 5;
+      symbol_map = let
+      in
+        (builtins.concatStringsSep "," mappings) + " Symbols Nerd Font";
+    };
+  };
 }
