5dc1cfaee47875d9d2448db35049ac641372edbd
Maddie <egonbold@gmx.de>
2025-07-20 16:17:58 +0200

    Nix generation: 57 current  2025-07-20 15:59:05  25.05.20250515.e06158e     6.12.28                          *

--- home-manager/fastfetch.nix
+++ home-manager/fastfetch.nix
@@ -3,12 +3,13 @@
   lib,
   ...
 }: {
-programs.fastfetch = {
-  enable = true;
-  settings = {
-    logo = {
-     source = "/home/maddie/Pictures/backrounds/fastfatch.png";
-      type = "kitty";
+  programs.fastfetch = {
+    enable = true;
+    settings = {
+      logo = {
+        source = "/home/maddie/Pictures/backrounds/fastfatch.png";
+        type = "kitty";
+      };
     };
   };
 }
