9b99dcff89b0ae01c3335930d9e0c46c007fb5fd
Maddie <egonbold@gmx.de>
2025-07-30 22:30:13 +0200

    Nix generation: 58 current  2025-07-30 22:29:36  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -193,6 +193,7 @@
     fastfetch
     hyfetch
     nushell
+    everest-mons
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
