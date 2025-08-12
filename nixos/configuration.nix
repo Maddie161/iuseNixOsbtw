30df89bf687a2c1e7e5cba01644c2832097b5d6d
Maddie <egonbold@gmx.de>
2025-08-12 22:32:38 +0200

    Nix generation: 60 current  2025-08-06 15:19:13  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -194,6 +194,7 @@
     nushell
     everest-mons
     masterpdfeditor
+    inputs.suyu.packages.x86_64-linux.suyu
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
