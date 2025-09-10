78c06f076da2e4d4cd86133104386405b274d6a1
Maddie <egonbold@gmx.de>
2025-09-10 14:18:43 +0200

    Nix generation: 63 current  2025-09-02 20:30:45  25.05.20250515.e06158e     6.12.28                          *

--- home-manager/fastfetch.nix
+++ home-manager/fastfetch.nix
@@ -10,6 +10,39 @@
         source = "/home/maddie/Pictures/backrounds/fastfetch.png";
         type = "kitty";
       };
+      modules = [
+        {
+                  type = "os";
+                  key = " ";
+                  keyColor = "purple";
+                }
+                {
+                  type = "packages";
+                  key = " ";
+                  keyColor = "purple";
+                }
+                {
+                  type = "shell";
+                  key = " ";
+                  keyColor = "purple";
+                }
+                {
+                  type = "terminal";
+                  key = " ";
+                  keyColor = "purple";
+                }
+                {
+                  type = "wm";
+                  key = "wm";
+                  keyColor = "purple";
+                }
+                "break"
+                {
+                  type = "uptime";
+                  key = " ";
+                  keyColor = "purple";
+                }
+      ]
     };
   };
 }
