0519be5abdc912574b5818f2501c3a9d163af5aa
Maddie <egonbold@gmx.de>
2025-09-29 20:18:28 +0200

    Nix generation: 70 current  2025-09-29 20:18:04  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -201,6 +201,7 @@
     helix
     btop
     kdePackages.dolphin
+    networkmanager
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
