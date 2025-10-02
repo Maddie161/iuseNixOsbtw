11b9cebe70c14c33f7ea4aacfedc154b1e85657f
Maddie <egonbold@gmx.de>
2025-10-02 22:24:26 +0200

    Nix generation: 72 current  2025-10-02 22:18:37  25.05.20250515.e06158e     6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -203,6 +203,7 @@
     kdePackages.dolphin
     bridge-utils
     nftables
+    dnsmasqcd i
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
