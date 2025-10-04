8fb43a119e7387d3cf7e476fa6fc772519d77817
maddie <madelinebold@gmail.com>
2025-10-04 23:32:52 +0200

    Nix generation: 9 current   2025-10-04 23:32:48  25.05.20250515.e06158e  6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -203,14 +203,14 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
-
+  services.openssh.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-   enableSSHSupport = true;
-  };
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
   # List services that you want to enable:
 
