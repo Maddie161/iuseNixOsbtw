582ceb6e6ad8dfae1d3ec3f046fe7779c9c8b22f
maddie <madelinebold@gmail.com>
2025-10-06 18:04:01 +0200

    Nix generation: 14 current  2025-10-06 18:03:59  25.05.20250515.e06158e  6.12.28                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -22,7 +22,9 @@
   ];
   security.polkit.enable = true;
   services.blueman.enable = true;
+  hardware.enableAllFirmware = true;
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.package = pkgs.bluez;
   hardware.bluetooth.powerOnBoot = true;
   hardware.graphics = {
     enable = true;
