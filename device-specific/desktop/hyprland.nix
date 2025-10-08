39ce71aeb67d1cabca9e4750c20a66aa639584ee
maddie <madelinebold@gmail.com>
2025-10-08 14:15:08 +0200

    Nix generation: 17 current  2025-10-06 19:02:55  25.05.20250515.e06158e  6.14.6                           *

--- device-specific/desktop/hyprland.nix
+++ device-specific/desktop/hyprland.nix
@@ -4,16 +4,18 @@
   inputs,
   ...
 }: {
-  settings = {
-    exec-once = [
-      "swaync"
-      "discordptb"
-      "steam"
-      "firefox"
-      "blueman-applet"
-      "waybar"
-      "hyprsunset --temperature 4500"
-      "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
-    ];
+  wayland.windowManager.hyprland = {
+    settings = {
+      exec-once = [
+        "swaync"
+        "discordptb"
+        "steam"
+        "firefox"
+        "blueman-applet"
+        "waybar"
+        "hyprsunset -t 4500"
+        "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
+      ];
+    };
   };
 }
--- device-specific/laptop/hyprland.nix
+++ device-specific/laptop/hyprland.nix
@@ -4,12 +4,15 @@
   inputs,
   ...
 }: {
-  settings {
-    exec-once = [
-      "swaync"
-      "waybar"
-      "hyprsunset --temperature 4500 -g 10"
-      "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
-    ];
+  wayland.windowManager.hyprland = {
+    settings = {
+      exec-once = [
+        "swaync"
+        "blueman-applet"
+        "waybar"
+        "hyprsunset -t 4500 -g 10"
+        "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
+      ];
+    };
   };
 }
