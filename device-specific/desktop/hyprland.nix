87497477428dd30fcd3c671e999a364aabb661ca
maddie <madelinebold@gmail.com>
2025-10-08 14:23:58 +0200

    Nix generation: 17 current  2025-10-06 19:02:55  25.05.20250515.e06158e  6.14.6                           *

--- device-specific/desktop/hyprland.nix
+++ device-specific/desktop/hyprland.nix
@@ -4,18 +4,16 @@
   inputs,
   ...
 }: {
-  wayland.windowManager.hyprland = {
-    settings = {
-      exec-once = ''
-        "swaync"
-        "discordptb"
-        "steam"
-        "firefox"
-        "blueman-applet"
-        "waybar"
-        "hyprsunset -t 4500"
-        "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
-      '';
-    };
+  wayland.windowManager.hyprland.settings = {
+    exec-once = ''
+      "swaync"
+      "discordptb"
+      "steam"
+      "firefox"
+      "blueman-applet"
+      "waybar"
+      "hyprsunset -t 4500"
+      "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
+    '';
   };
 }
--- device-specific/laptop/hyprland.nix
+++ device-specific/laptop/hyprland.nix
@@ -4,17 +4,14 @@
   inputs,
   ...
 }: {
-  wayland.windowManager.hyprland = {
-    settings = {
-      # exec-once should be a single string with commands separated by newlines
-      exec-once = ''
-        swaync
-        blueman-applet
-        waybar
-        hyprsunset -t 4500 -g 10
-        ${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1
-      '';
-    };
+  wayland.windowManager.hyprland.settings = {
+    # exec-once should be a single string with commands separated by newlines
+    exec-once = ''
+      swaync
+      blueman-applet
+      waybar
+      hyprsunset -t 4500 -g 10
+      ${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1
+    '';
   };
-
 }
