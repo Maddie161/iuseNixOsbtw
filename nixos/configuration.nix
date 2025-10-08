264aae461414c0268d63c02e0feb86abf058fe3a
maddie <madelinebold@gmail.com>
2025-10-08 20:17:56 +0200

    Nix generation: 18 current  2025-10-08 20:17:53  25.05.20250515.e06158e  6.14.6                           *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -200,6 +200,7 @@
     nftables
     dnsmasq
     ethtool
+    zip
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
