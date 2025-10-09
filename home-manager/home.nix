498726c61c671ed20d964927c89139581a7f0514
maddie <madelinebold@gmail.com>
2025-10-09 18:05:47 +0200

    Nix generation: 20 current  2025-10-08 20:24:28  25.05.20250515.e06158e  6.14.6                           *

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -35,12 +35,6 @@
       shell = "nu";
       notify_on_cmd_finish = "unfocused";
     };
-    exec-once = [
-      "swaync"
-      "waybar"
-      "hyprsunset --temperature 4500 -g 10"
-      "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
-    ];
   };
 
   programs.nushell = {
