08746e4dc739c3f0e617af2ef2c870899b4c7514
Maddie <egonbold@gmx.de>
2025-10-18 23:45:14 +0200

    Nix generation: 85 current  2025-10-09 18:18:00  25.05.20250515.e06158e     6.14.6                           *

--- device-specific/laptop/hyprland.nix
+++ device-specific/laptop/hyprland.nix
@@ -5,7 +5,7 @@
       "blueman-applet"
       "waybar"
       "hyprsunset -t 4500 -g 20"
-      "&term"
+      "kitty nu -e nu ~Documents/wifi.nu"
       "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
     ];
   };
