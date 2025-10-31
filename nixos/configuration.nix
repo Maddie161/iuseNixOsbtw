911be9c2233846d415681b695f27164ab0599c22
maddie <madelinebold@gmail.com>
2025-10-31 17:34:26 +0100

    Nix generation: 29 current  2025-10-31 17:34:23  25.05.20250515.e06158e  6.14.6                           *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -204,6 +204,7 @@
     dnsmasq
     ethtool
     unzip
+    flatpak
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
