2789e6027c8045faea0f0c5b71b887a3c111b87e
maddie <madelinebold@gmail.com>
2025-10-31 17:33:01 +0100

    Nix generation: 28 current  2025-10-31 17:32:58  25.05.20250515.e06158e  6.14.6                           *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -142,6 +142,11 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = ["multi-user.target"];
+    path = [pkgs.flatpak];
+    script = ''flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo'';
+  };
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
