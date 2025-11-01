983d966889d56067a40de7c8a17cd3a49a2b441d
maddie <madelinebold@gmail.com>
2025-11-01 23:39:17 +0100

    Nix generation: 37 current  2025-11-01 23:39:13  25.05.20250515.e06158e  6.14.6                           *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -222,7 +222,7 @@
     winetricks
     vulkan-tools
     glxinfo
-    python2
+    python314
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
