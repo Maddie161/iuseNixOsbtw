004b427f3a86063fca0e3714c4a114d722c29299
maddie <madelinebold@gmail.com>
2025-11-19 08:20:08 +0100

    Nix generation: 38 current  2025-11-18 20:33:45  25.05.20250515.e06158e  6.14.6                           *

--- home-manager/fastfetch.nix
+++ home-manager/fastfetch.nix
@@ -7,7 +7,7 @@
     enable = true;
     settings = {
       logo = {
-        source = "/home/maddie/Pictures/backrounds/fastfetch1.jpg";
+        source = "/home/maddie/Pictures/backrounds/fastfetch2.jpg";
         type = "kitty";
         position = "left";
       };
