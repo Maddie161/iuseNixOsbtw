6754b29b673aa28b6a0c4d6221c8a926dc48c9fc
maddie <madelinebold@gmail.com>
2025-11-23 17:36:13 +0100

    Nix generation: 39 current  2025-11-23 17:36:09  25.05.20250515.e06158e  6.14.6                           *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -224,6 +224,7 @@
     glxinfo
     python314
     cmatrix
+    libsForQt5.kolourpaint
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
