0d8d58e191c5ae3f94c2b4405a3561d0df4e37d3
maddie <madelinebold@gmail.com>
2026-01-15 17:50:05 +0100

    Nix generation: 57 2026-01-15 17:49:56 kernel 6.18.2 Home Generation: 2026-01-05 16:02 : id 36 -> /nix/store/6803hyn9fy5ss6aik588sjb1zn4py8mr-home-manager-generation (current)

--- device-specific/desktop/configuration.nix
+++ device-specific/desktop/configuration.nix
@@ -9,7 +9,7 @@
     ../../nixos/configuration.nix
     ./hardware-configuration.nix
   ];
-  networking.hostName = "desktop";
+  networking.hostName = "48b5becd-f98c-4897-98aa-be37eecb6a68";
   environment.systemPackages = with pkgs; [
     kdePackages.kolourpaint
     legendary-gl
