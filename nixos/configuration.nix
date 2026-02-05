2f7689d6e47083115935ed8036fcaa6666139364
maddie <madelinebold@gmail.com>
2026-02-05 21:39:34 +0100

    Nix generation: 66 2026-02-05 21:39:31 kernel 6.18.6

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -206,6 +206,7 @@
     winetricks
     mesa-demos
     cmatrix
+    cliphist
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
