96d967f17cbfd6d2cf33ebcbc74271f4fb27f48a
maddie <madelinebold161@gmail.com>
2026-06-16 08:57:28 +0200

    Nix generation: 126 2026-06-16 08:57:20 kernel 7.0.12 Home Generation: 2026-06-14 20:45 : id 88 -> /nix/store/6ibvl6wr62j2yx9cq3bqpsdina3x22p5-home-manager-generation (current)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -217,6 +217,7 @@
     unrar-free
     directx-headers
     ryubing
+    rPackages.canvasXpress
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
