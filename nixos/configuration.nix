fbe446ca90abf411d93c92ed840ce74c4c79c932
maddie <madelinebold161@gmail.com>
2026-08-15 20:12:34 +0200

    Nix generation: 140 2026-08-15 20:12:27 kernel 7.1.2 Home Generation: 2026-08-08 18:42 : id 104 -> /nix/store/jgrm67m3q90fadm7ir5vqllfkyjr310g-home-manager-generation (current)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -215,7 +215,7 @@
     directx-headers
     ryubing
     protonup-rs
-    everest
+    everest-mons
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
