f01f79f4a2a7e22051ca914b88fc582e02526d68
maddie <madelinebold161@gmail.com>
2026-08-15 21:27:51 +0200

    Nix generation: 143 2026-08-15 21:24:18 kernel 7.1.2

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -215,7 +215,7 @@
     directx-headers
     ryubing
     protonup-rs
-    fuse
+    fuse2
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
