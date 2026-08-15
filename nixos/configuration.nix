49123b4a9254f6faf5514d4e77bb7f74e875cd6b
maddie <madelinebold161@gmail.com>
2026-08-15 22:58:48 +0200

    Nix generation: 149 2026-08-15 22:58:46 kernel 7.1.2

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -220,6 +220,7 @@
     love
     flatpak
     glibc
+    bubblewrap
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
