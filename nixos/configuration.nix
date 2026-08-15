e8cf80113d2633a294b19b22f709cbe8e0708be3
maddie <madelinebold161@gmail.com>
2026-08-15 23:13:34 +0200

    Nix generation: 150 2026-08-15 23:13:32 kernel 7.1.2

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -221,6 +221,9 @@
     flatpak
     glibc
     bubblewrap
+    (olympus.override {
+        celesteWrapper = "steam-run";
+      })
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
   ];
