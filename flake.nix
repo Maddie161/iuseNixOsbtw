44b2b405344d7feedf4e88a37707e9b18d6b381c
maddie <madelinebold161@gmail.com>
2026-08-15 20:50:05 +0200

    Nix generation: 142 2026-08-15 20:49:58 kernel 7.1.2 Home Generation: 2026-08-08 18:42 : id 104 -> /nix/store/jgrm67m3q90fadm7ir5vqllfkyjr310g-home-manager-generation (current)

--- flake.nix
+++ flake.nix
@@ -10,7 +10,10 @@
       url = "github:outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    olympus = {
+      url = "github:spencerpogo/olympus-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
